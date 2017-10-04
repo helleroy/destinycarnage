c80522cd881d03c61ae5b28c3eb9b64b5803feeb
Henrik Hellerøy <helleroy@gmail.com>
2017-10-04 16:35:46 +0200

    Use react router to handle redirects with auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom'
+import Auth from './Auth';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,12 +13,9 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    Test! :D
-                </p>
+                <BrowserRouter>
+                    <Route path="/" component={Auth}/>
+                </BrowserRouter>
             </div>
         );
     }
