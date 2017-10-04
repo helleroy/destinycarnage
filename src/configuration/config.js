7143ae232f85c42201d8b611acd4cce8b809b00d
Henrik Hellerøy <helleroy@gmail.com>
2017-10-04 23:24:55 +0200

    Attempt to prefix platform url with www to prevent preflight redirect

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -1,3 +1,3 @@
 export const API_KEY = process.env.REACT_APP_API_KEY;
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-export const BUNGIE_API_ROOT = "https://bungie.net/Platform";
+export const BUNGIE_API_ROOT = "https://www.bungie.net/Platform";
